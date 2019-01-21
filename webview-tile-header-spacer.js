ba893906734b60cd68e99d82c656677befbda4cb
Clint <clintp1@gmail.com>
2019-01-21 11:29:42 +1100

    convert to lit-element base

--- webview-tile-header-spacer.js
+++ webview-tile-header-spacer.js
@@ -1,9 +1,12 @@
-import { PolymerElement } from './node_modules/@polymer/polymer/polymer-element.js';
-const $_documentContainer = document.createElement('template');
+import { LitElement, html } from './node_modules/lit-element/lit-element.js';
 
-$_documentContainer.innerHTML = `<dom-module id="moduware-header-spacer">
-  <template id="styles">
-    <style>
+/**
+ * @extends HTMLElement
+ */
+class ModuwareWebViewTileHeaderSpacer extends LitElement {
+  render() {
+    return html`
+     <style>
       :host {
         --ios-header-height: 44px;
         --android-header-height: 55px;
@@ -18,41 +21,45 @@ $_documentContainer.innerHTML = `<dom-module id="moduware-header-spacer">
       :host([platform="android"]) {
         height: var(--android-header-height);
       }
-    </style>
-  </template>
-
-  
-</dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
-
-/**
- * @extends HTMLElement
- */
-class ModuwareWebViewTileHeaderSpacer extends PolymerElement {
+    </style> 
+    `
+  }
   static get is() { return 'moduware-header-spacer'; }
   static get properties() {
     return {
       platform: {
         type: String,
-        reflectToAttribute: true,
-        value: function() {
-          let userAgent = navigator.userAgent || navigator.vendor || window.opera;
-          // Windows Phone must come first because its UA also contains "Android"
-          if (/windows phone/i.test(userAgent)) {
-            return 'windows-phone';
-          }
-          if (/android/i.test(userAgent)) {
-            return 'android';
-          }
-          // iOS detection from: http://stackoverflow.com/a/9039885/177710
-          if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return 'ios';
-          }
-          return 'unknown';
-        }
+        reflect: true
       }
     };
+  }
+
+  /**
+    * Instance of the element is created/upgraded. Useful for initializing
+    * state, set up event listeners, create shadow dom.
+    * @constructor
+    */
+  constructor() {
+    super();
+  
+    this.platform = this.getPlatform();
+    console.log('this.platform', this.platform);
+  }
+
+  getPlatform () {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+      return 'windows-phone';
+    }
+    if (/android/i.test(userAgent)) {
+      return 'android';
+    }
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return 'ios';
+    }
+    return 'unknown';
   }
 }
 
